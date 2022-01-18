6d18dc2b48b2f12134d6ee670b23730415219671
spreadmycode <91585048+spreadmycode@users.noreply.github.com>
2022-01-18 11:36:36 -0800

    beta version finished

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const cron = require('node-cron');
+const shell = require('shelljs');
+
+cron.schedule('50 23 * * *', async function() {
+    console.log(`--------------- ${new Date().toLocaleString()} -----------------`);
+    console.log('Starting Airdrop');
+    
+    if (shell.exec(`ts-node ${__dirname}/airdrop.ts`).code !== 0) {
+        shell.exit(1);
+    }
+    else {
+        shell.echo('Database backup complete');
+    }
+});
